08bf5acda70b7a2d25b874507ae06e5748bf896c
Chloe <chloeemstokes@gmail.com>
2022-01-26 19:38:44 +0000

    Added comments

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,16 +2,12 @@ require_relative "../lib/dockingstation.rb"
 
 describe DockingStation do
     describe "#release_bike" do
-      it { is_expected.to respond_to :release_bike }
+      it { is_expected.to respond_to :release_bike } # ASSERT
 
       it "releases working bikes" do
-        bike = subject.release_bike
-        expect(bike).to be_working
+        bike = subject.release_bike # ACT
+        expect(bike).to be_working # ASSERT
       end
     end
-
-    # describe "#working?" do
-
-    # end
 end
 
